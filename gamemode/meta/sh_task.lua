e185651504c5c0eb6867f42aabc31d939acdb67d
LilSumac <msearch091@gmail.com>
2017-10-07 16:27:08 -0400

    Working on tasks.

--- gamemode/meta/sh_task.lua
+++ gamemode/meta/sh_task.lua
@@ -1,3 +1,28 @@
 defineMeta_start("Task");
 
+function META:Start()
+    self.Conditions = {};
+
+end
+
+function META:Pause()
+    // if not time-based, ignore
+end
+
+function META:Update()
+
+end
+
+function META:Fail()
+
+end
+
+function META:GetProgress()
+    // return number, table of progress(?)
+end
+
+function META:AddListener(plys)
+
+end
+
 defineMeta_end();
--- gamemode/services/sh_task.lua
+++ gamemode/services/sh_task.lua
@@ -6,19 +6,111 @@ SVC.Author = "LilSumac";
 SVC.Desc = "Simple framework for implementing code-based tasks with progress, feedback, and callbacks.";
 
 -- Service storage.
+local tasks = {};
+local activeTasks = getNonVolatileEntry("CTask_ActiveTasks", EMPTY_TABLE);
+
 SVC.Tasks = {};
 SVC.ActiveTasks = getNonVolatileEntry("CTask_ActiveTasks", EMPTY_TABLE);
 
 function SVC:AddTask(task)
 
+    task.ID = task.ID;
+    task.AllowMultiple = task.AllowMultiple or false;
+    task.Conditions = {};
+
 end
 
-function SVC:AddTaskCondition(id, cond, begin, thresh)
+function SVC:AddTaskCondition(id, cond, begin, finish)
+    if !tasks[id] then
+        MsgErr("NilEntry", id);
+        return;
+    end
 
+    local task = tasks[id];
+    if task.Conditions[cond] then
+        MsgErr("DupEntry", cond);
+        return;
+    end
+
+    local newCond = {};
+    newCond.__index = newCond;
+    newCond.Type = TASK_NUMERIC;
+    newCond.Start = begin;
+    newCond.Finish = finish;
+    newCond.Update = function(_self, value)
+        if _self.Type == TASK_NUMERIC then
+            _self.Value = _self.Value + value;
+        else
+            _self.Value = value;
+        end
+    end
+    newCond.Progress = function(_self)
+        if _self.Type == TASK_NUMERIC then
+            return ((_self.Value - _self.Start) / (_self.Finish - _self.Start));
+        else
+            return _self.Value;
+        end
+    end
+    newCond.IsFinished = function(_self)
+        if _self.Type == TASK_NUMERIC then
+            return _self.Value >= _self.Finish;
+        else
+            return _self.Value == _self.Finish;
+        end
+    end
 end
 
 function SVC:StartTask(id)
+    if !tasks[id] then
+        MsgErr("NilEntry", id);
+        return;
+    end
 
+    local task = tasks[id];
+    local newID = string.random(8, CHAR_ALPHANUM, "task_");
+    while activeTasks[newID] do
+        newID = string.random(8, CHAR_ALPHANUM, "task_");
+    end
+
+    activeTasks[newID] = {};
+    activeTasks[newID].TaskID = id;
+    activeTasks[newID].Conds = {};
+    for _id, cond in pairs(task.Conditions) do
+        activeTasks[newID].Conds[_id] = setmetatable({}, cond);
+    end
+
+    return newID;
 end
+
+function SVC:GetActiveTask(id)
+    if !id then
+        MsgErr("NilArgs", "id");
+        return;
+    end
+    if !activeTasks[id] then
+        MsgErr("NilEntry", id);
+        return;
+    end
+
+    return activeTasks[id];
+end
+
+function SVC:UpdateTask(id, cond, value)
+    if !activeTasks[id] then
+        MsgErr("TaskNotActive", id);
+        return;
+    end
+
+    local curTask = activeTasks[id];
+    if !curTask.Conds[cond] then
+        MsgErr("NilEntry", cond);
+        return;
+    end
+
+    curTask.Conds[cond]:Update(value);
+end
+
+-- Custom errors.
+addErrType("TaskNotActive", "No task with that ID is active! (%s)");
 
 defineService_end();
