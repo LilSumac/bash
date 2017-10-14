cf4369d3cb2ddb59d81671f01f8b1c87ce02c8b2
LilSumac <msearch091@gmail.com>
2017-10-14 14:18:37 -0400

    Reduce overhead of IsEmpty function.

--- gamemode/libraries/sh_table.lua
+++ gamemode/libraries/sh_table.lua
@@ -2,6 +2,5 @@ local table = table;
 
 function table.IsEmpty(tab)
     if !tab or type(tab) != "table" then return false; end
-    for _, __ in pairs(tab) do return false; end
-    return true;
+    return next(tab) == nil;
 end
